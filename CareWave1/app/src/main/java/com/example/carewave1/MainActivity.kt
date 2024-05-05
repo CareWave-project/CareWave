7fee63abce42a8315b1e9871ddc200a3a74eb888
GeorlitGeorge <83900643+GeorlitGeorge@users.noreply.github.com>
2024-05-05 11:48:14 +0530

    Update MainActivity.kt

--- CareWave1/app/src/main/java/com/example/carewave1/MainActivity.kt
+++ CareWave1/app/src/main/java/com/example/carewave1/MainActivity.kt
@@ -3,24 +3,22 @@ package com.example.carewave1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
+import android.os.Handler
 import com.google.firebase.auth.FirebaseAuth
 
-
 class MainActivity : AppCompatActivity() {
-
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.splashscreen) // Use splash screen layout
 
-
-        // Dummy function for sending data to the database (You can remove this if not needed)
-        fun sendData(view: View) {
-            // Write a message to the database
-            // This function is not related to Firebase Authentication, you may remove it if not needed.
-        }
+        // Display splash screen for 3 seconds
+        Handler().postDelayed({
+            // Start the LoginActivity after 3 seconds
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            //finish() // Finish the MainActivity to prevent returning to it via back navigation
+        }, 3000) // Delay for 3 seconds (3000 milliseconds)
     }
 }
